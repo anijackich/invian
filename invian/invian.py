b112e9431ac326904b0f03c33b1abce38598309c
anijackich <anijackich@gmail.com>
2024-04-27 14:22:50 +0300

    Add snapshots concept logic

--- invian/invian.py
+++ invian/invian.py
@@ -1,3 +1,45 @@
+import json
+from kafka import KafkaConsumer
+
+from .types import RoadSnapshot, Car, _RawMessage
+
+
 class Invian:
-    def __init__(self):
-        pass
+    def __init__(self, server: str, group_id: str, topics: list[str]):
+        self.kafka_consumer = KafkaConsumer(*topics,
+                                            bootstrap_servers=server,
+                                            group_id=group_id)
+
+    def __del__(self):
+        self.kafka_consumer.close()
+
+    def _get_raw_stream(self):
+        return (
+            _RawMessage(json.loads(msg.value.decode()))
+            for msg in self.kafka_consumer
+        )
+
+    def get_stream(self, buffer_length: int = 4):
+        buffer = {}
+
+        current_ts = 0
+        current_msgs = []
+
+        for msg in self._get_raw_stream():
+            if msg.unix_millis != current_ts:
+                buffer[current_ts] = buffer[current_ts] + current_msgs \
+                    if current_ts in buffer else current_msgs
+                current_msgs.clear()
+                current_ts = msg.unix_millis
+
+            current_msgs.append(msg)
+
+            if len(buffer) >= buffer_length:
+                msgs = buffer.pop(min(buffer.keys()))
+                yield RoadSnapshot(
+                    timestamp=msgs[0].unix_millis,
+                    cars=set(
+                        Car(coords=msg.center,
+                            transport_type=msg.cls) for msg in msgs
+                    )
+                )
--- invian/types.py
+++ invian/types.py
@@ -1,7 +1,38 @@
-class Transport:
+from enum import Enum
+from dataclasses import dataclass
+
+
+class Transport(Enum):
     MOTORCYCLE = 0
     CAR = 1
     CAR_WITH_TRAILER = 2
     TRUCK = 3
     ROAD_TRAIN = 4
     BUS = 5
+
+
+@dataclass
+class Car:
+    coords: tuple[int, int]
+    transport_type: Transport
+
+    def __hash__(self):
+        return hash((self.coords, self.transport_type))
+
+
+@dataclass
+class RoadSnapshot:
+    timestamp: int
+    cars: set[Car]
+
+
+@dataclass
+class _RawMessage:
+    unix_millis: int
+    center: tuple[int, int]
+    cls: Transport
+
+    def __init__(self, data: dict):
+        self.unix_millis = data['unix_millis']
+        self.center = tuple(data['center'])
+        self.cls = Transport(data['class'])
